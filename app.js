03d18879375dc337da760a481fac6c3cbad95222
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-10-25 06:58:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
+
+  // Sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+  // TIme display
+  const timeDisplay = document.querySelector('.time-display');
+  // Get length of the outline
+  const outlineLength = outline.getTotalLength();
+  // Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  // Play sound
+  play.addEventListener('click', function () {
+    checkPlaying(song);
+  });
+
+  // Create a function specific to stop and play the sounds
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = './svg/pause.svg';
+    } else {
+      song.pause();
+      video.pause();
+      play.src = './svg/play.svg';
+    }
+  };
+
+  // We can animated the circle
+  song.ontimeupdate = function () {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+    // Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    // Animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`
+  };
+};
+
+app();
